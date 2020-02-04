fa7429ae9f02c898bcb54d71b4ed2a8b322f9320
Kailash Pasumarthy <KPasumarthy@gmail.com>
2020-02-04 07:25:16 -0500

    KP : Added Python Input & Request Headers for APIs

--- script.py
+++ script.py
@@ -1,42 +1,51 @@
-#KP : Import 'sys' Commands
+# KP : Import 'sys' Commands
 import sys
+import math
+import os
 
-#KP : import requests #KP : Run Command : 'pip install requests' : Installs 'requests' library
+# KP : import requests #KP : Run Command : 'pip install requests' : Installs 'requests' library
 import requests
 
-#KP : Import 'print' command
+# KP : Import 'print' commandcl
 print(sys.version)
 print(sys.executable)
 
-#KP : define 'greet()' 
+# KP : define 'greet()'
 def greet(who_to_greet):
-    greeting = 'KP : Hello, {}'.format(who_to_greet)
+    # test = "Test"
+    greeting = "KP : Hello, {}".format(who_to_greet)
     return greeting
 
-#KP : Print 'greet()'
-print(greet('World'))
-print(greet('Kailash'))
 
-#KP : Initialize Local API Requests - With OUT Headers - http:// 500 Error Response
+# KP : Print 'greet()'
+print(greet("World"))
+print(greet("Kailash"))
+
+# KP : Get Input and Print
+name = input("Your Name ? >> ")
+print("KP : Hello, ", name)
+
+# KP : Initialize Local API Requests - With OUT Headers - http:// 500 Error Response
 url = "http://kpmvcwebapis.com/api/Persons/27"
 r = requests.get(url)
 print("KP : " + url + " Status Code : " + str(r.status_code))
 
-#KP : Initialize Local API Requests - With Headers - http:// 200 OK Response
+# KP : Initialize Local API Requests - With Headers - http:// 200 OK Response
 url = "http://kpmvcwebapis.com/api/Persons/27"
-headers = { 'content-type': 'application/json',
-            'accept': 'text/html, application/xhtml+xml, application/xml; q=0.9, */*; q=0.8',
-            'accept-Encoding': 'gzip, deflate',
-            'accept-language' : 'en-US, en; q=0.5',
-            'Connection':'Keep-Alive'}
-response = requests.get(url,  headers=headers)
-#data = r.json();
+headers = {
+    "content-type": "application/json",
+    "accept": "text/html, application/xhtml+xml, application/xml; q=0.9, */*; q=0.8",
+    "accept-Encoding": "gzip, deflate",
+    "accept-language": "en-US, en; q=0.5",
+    "Connection": "Keep-Alive",
+}
+response = requests.get(url, headers=headers)
 print("KP : " + url + " Status Code : " + str(response.status_code))
-print(response.text);
+print(response.text)
 
-#KP : Initialize External API Requests
+# KP : Initialize External API Requests
 url = "https://www.cnn.com/"
 response = requests.get(url)
 print("KP : " + url + " Status Code : " + str(response.status_code))
-#print(response.text);
+# print(response.text);
 
