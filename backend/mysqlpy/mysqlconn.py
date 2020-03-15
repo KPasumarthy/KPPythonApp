b94031ef7ec5e99bb2401f4497db1434e3d4f508
Kailash Pasumarthy <KPasumarthy@gmail.com>
2020-03-15 08:15:09 -0400

    KP : MySQL Connection & SQL Statement Execution OK!

--- backend/mysqlpy/mysqlconn.py
+++ backend/mysqlpy/mysqlconn.py
@@ -1,11 +1,10 @@
 # KP : Import 'MySQL' Connector module
 import mysql.connector
-#import kppylogs.kplogger_module
-#from kppylogs  import *
+#import kppylogs
 #import kppylogs.auxiliary_module
 #import kppylogs.kplogger_module
 
-#KP : Create 'MySQL' DB Connection
+#KP : 'MySQL' DB Connection 
 print("KP : mysqlconn.py :  Python 'MySQL' DB Connection" )
 mysqldb = mysql.connector.connect(
     host='localhost', 
@@ -14,3 +13,23 @@ mysqldb = mysql.connector.connect(
     passwd='(svcP@33word)'
 )
 print("KP : mysqlconn.py :  Python Established 'MySQL' DB Connection Successfully" )
+
+#KP : 'MySQL' Define Function to get Connection
+def getMySQLConn():    
+    return mysqldb
+
+#KP : 'MYSQL' DB Select Statement
+def mySqlSelect(mySqlStmnt):
+    print("KP : mysqlconn.py :  Execute 'MySQL' Statment!" )
+    mycursor = mysqldb.cursor()
+    mycursor.execute(mySqlStmnt)
+    mySqlResult = mycursor.fetchall()
+    print("KP : mysqlconn.py :  Python Executed 'MySQL' Statment Successfully!" )
+    return mySqlResult
+
+
+#KP : MySQL Statement to Select All Cities
+sqlStmnt4Cities = "Select * From world.city"
+cities = mySqlSelect(sqlStmnt4Cities)
+for city in cities:
+    print(city)
